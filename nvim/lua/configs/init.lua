d96bcaa88d318a3784627a88f2e0a45a3340c3e9
wildduck <wezonaser50@gmail.com>
2024-12-29 14:16:32 +0200

    feat: new configs

--- nvim/lua/configs/init.lua
+++ nvim/lua/configs/init.lua
@@ -1,39 +1,39 @@
--- NOTE: importing files
- require 'configs.lualine'
- require('configs.catppuccin')
- require 'configs.cloak'
- require 'configs.harpoon'
- require 'configs.colors'
- require 'configs.zenmode'
- require 'configs.comment'
- require 'configs.undotree'
- require 'configs.treesitter'
- require 'configs.trouble'
- require 'configs.nvim-tree'
- require 'configs.todo-comments'
- require 'configs.telescope'
- require 'configs.fugitive'
- require 'configs.lsp'
- require 'configs.null-ls'
- require 'configs.cmp'
- 
- require 'configs.tmux-naviagate'
- require 'configs.presence'
- require 'configs.refactoring'
- require 'configs.mini'
- require 'configs.hacker'
- require 'configs.randiverse'
- require 'configs.nvim-ts-autotag'
- require 'configs.silicon'
- require 'configs.transparent'
- require 'configs.gitsigns'
- require 'configs.conform'
- require 'configs.which-key'
- 
- -- require('configs.airline')
- -- require('configs.autopairs')
- -- require('configs.autotag')
- require 'configs.iblconf'
- require 'configs.dap'
- -- require 'configs.obsidian'
- require 'configs.dbee'
+---- NOTE: importing files
+-- require 'configs.lualine'
+-- require('configs.catppuccin')
+-- require 'configs.cloak'
+-- require 'configs.harpoon'
+-- require 'configs.colors'
+-- require 'configs.zenmode'
+-- require 'configs.comment'
+-- require 'configs.undotree'
+-- require 'configs.treesitter'
+-- require 'configs.trouble'
+-- require 'configs.nvim-tree'
+-- require 'configs.todo-comments'
+-- require 'configs.telescope'
+-- require 'configs.fugitive'
+-- require 'configs.lsp'
+-- require 'configs.null-ls'
+-- require 'configs.cmp'
+-- 
+-- require 'configs.tmux-naviagate'
+-- require 'configs.presence'
+-- require 'configs.refactoring'
+-- require 'configs.mini'
+-- require 'configs.hacker'
+-- require 'configs.randiverse'
+-- require 'configs.nvim-ts-autotag'
+-- require 'configs.silicon'
+-- require 'configs.transparent'
+-- require 'configs.gitsigns'
+-- require 'configs.conform'
+-- require 'configs.which-key'
+-- 
+-- -- require('configs.airline')
+-- -- require('configs.autopairs')
+-- -- require('configs.autotag')
+-- require 'configs.iblconf'
+-- require 'configs.dap'
+-- -- require 'configs.obsidian'
+-- require 'configs.dbee'
--- nvim/lua/wild-duck/lazy.lua
+++ nvim/lua/wild-duck/lazy.lua
@@ -106,322 +106,322 @@ local plugins = {
        },
    },
 
-   -- Telescope
-   {
-       'nvim-telescope/telescope.nvim',
-       event = 'VimEnter',
-       branch = '0.1.x',
-       dependencies = {
-           'nvim-lua/plenary.nvim',
-           {
-               'nvim-telescope/telescope-fzf-native.nvim',
-               -- NOTE: If you are having trouble with this installation,
-               --       refer to the README for telescope-fzf-native for more instructions.
-               build = 'make',
-               cond = function()
-                   return vim.fn.executable 'make' == 1
-               end,
-           },
-           { 'nvim-telescope/telescope-ui-select.nvim' },
-
-           -- Useful for getting pretty icons, but requires a Nerd Font.
-           { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
-       },
-   },
-
-   -- Codeium completion
-   {
-       'Exafunction/codeium.vim',
-   },
-
-   --Null-ls
-   {
-       'jose-elias-alvarez/null-ls.nvim',
-       event = 'VeryLazy',
-   },
-
-   -- Git related plugins
-   { 'tpope/vim-fugitive' },
-
-   -- Nvim tree
-   {
-       'nvim-tree/nvim-tree.lua',
-       dependencies = { 'nvim-tree/nvim-web-devicons' },
-       cmd = { 'NvimTreeToggle', 'NvimTreeFocus' },
-   },
-
-   -- Fancey vim
-   {
-       'eandrju/cellular-automaton.nvim',
-   },
-
-   -- presence
-   {
-       'andweeb/presence.nvim',
-   },
-
-   {
-       'vyfor/cord.nvim',
-       build = './build',
-       event = 'VeryLazy',
-       opts = {},
-   },
-
-   -- LSP
-   {
-       'neovim/nvim-lspconfig',
-       dependencies = {
-           -- Automatically install LSPs and related tools to stdpath for neovim
-           'williamboman/mason.nvim',
-           'williamboman/mason-lspconfig.nvim',
-           'WhoIsSethDaniel/mason-tool-installer.nvim',
-           'MunifTanjim/prettier.nvim',
-
-           -- Useful status updates for LSP.
-           -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-           { 'j-hui/fidget.nvim', opts = {} },
-       },
-   },
-   {
-       -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
-       -- used for completion, annotations and signatures of Neovim apis
-       'folke/lazydev.nvim',
-       ft = 'lua',
-       opts = {
-           library = {
-               -- Load luvit types when the `vim.uv` word is found
-               { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-           },
-       },
-   },
-   { 'Bilal2453/luvit-meta', lazy = true },
-
-   -- Autocompletion
-   {
-       'hrsh7th/nvim-cmp',
-       event = 'InsertEnter',
-       dependencies = {
-           -- Snippet Engine & its associated nvim-cmp source
-           {
-               'L3MON4D3/LuaSnip',
-               build = (function()
-                   -- Build Step is needed for regex support in snippets
-                   -- This step is not supported in many windows environments
-                   -- Remove the below condition to re-enable on windows
-                   if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
-                       return
-                   end
-                   return 'make install_jsregexp'
-               end)(),
-           },
-           -- LSP Support
-           { 'neovim/nvim-lspconfig' },
-           { 'williamboman/mason.nvim' },
-           { 'williamboman/mason-lspconfig.nvim' },
-
-           -- Autocompletion
-           { 'hrsh7th/cmp-buffer' },
-           { 'hrsh7th/cmp-path' },
-           { 'hrsh7th/cmp-cmdline' },
-           { 'saadparwaiz1/cmp_luasnip' },
-           { 'hrsh7th/cmp-nvim-lsp' },
-           { 'hrsh7th/cmp-nvim-lua' },
-
-           -- Snippets
-           { 'L3MON4D3/LuaSnip' },
-           { 'rafamadriz/friendly-snippets' },
-       },
-   },
-
-   -- Tmux navigate
-   {
-       'christoomey/vim-tmux-navigator',
-       lazy = false,
-   },
-
-   -- Detect tabstop and shiftwidth automatically
-   {
-       'ThePrimeagen/refactoring.nvim',
-       dependencies = {
-           'nvim-lua/plenary.nvim',
-           'nvim-treesitter/nvim-treesitter',
-       },
-   },
-
-   {
-       -- Add indentation guides even on blank lines
-       'lukas-reineke/indent-blankline.nvim',
-       -- Enable `lukas-reineke/indent-blankline.nvim`
-       -- See `:help ibl`
-       main = 'ibl',
-       opts = {},
-   },
-   { -- Linting
-       'mfussenegger/nvim-lint',
-       event = { 'BufReadPre', 'BufNewFile' },
-       config = function()
-           require 'configs.linting'
-       end,
-   },
-
-   { -- Autoformat
-       'stevearc/conform.nvim',
-       event = { 'BufWritePre' },
-       cmd = { 'ConformInfo' },
-       keys = {
-           {
-               '<leader>f',
-               function()
-                   require('conform').format { async = true, lsp_format = 'fallback' }
-               end,
-               mode = '',
-               desc = '[F]ormat buffer',
-           },
-       },
-   },
-
-   { -- Collection of various small independent plugins/modules
-       'echasnovski/mini.nvim',
-       configs = require 'configs.mini',
-   },
-   {                       -- Useful plugin to show you pending keybinds.
-       'folke/which-key.nvim',
-       event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-   },
-
-   {
-       'xiyaowong/transparent.nvim',
-   },
-
-   {
-       'letieu/hacker.nvim',
-   },
-   {
-       'ty-labs/randiverse.nvim',
-       version = '*', -- Use for stability; omit to use `main` branch for the latest features
-   },
-   -- Auto pairs
-   -- {
-   --     "windwp/nvim-autopairs",
-   --     event = "InsertEnter",
-   --     opts = {}, -- this is equalent to setup({}) function
-   -- },
-   -- "tmsvg/pear-tree",
-
-   -- Auto tag
-   {
-       'windwp/nvim-ts-autotag',
-   },
-
-   -- selicon snap
-   {
-       'michaelrommel/nvim-silicon',
-       lazy = true,
-       cmd = 'Silicon',
-   },
-   -- {
-   --   'iamcco/markdown-preview.nvim',
-   --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
-   --   ft = { 'markdown' },
-   --   build = function()
-   --     vim.fn['mkdp#util#install']()
-   --   end,
-   -- },
-   {
-       'davidmh/mdx.nvim',
-       config = true,
-       dependencies = { 'nvim-treesitter/nvim-treesitter' },
-   },
-   {
-       'epwalsh/obsidian.nvim',
-       version = '*', -- recommended, use latest release instead of latest commit
-       lazy = true,
-       ft = 'markdown',
-       -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-       -- event = {
-       --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-       --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-       --   -- refer to `:h file-pattern` for more examples
-       --   "BufReadPre path/to/my-vault/*.md",
-       --   "BufNewFile path/to/my-vault/*.md",
-       -- },
-       dependencies = {
-           -- Required.
-           'nvim-lua/plenary.nvim',
-
-           -- see below for full list of optional dependencies 👇
-       },
-   },
-   {
-       'rust-lang/rust.vim',
-       ft = 'rust',
-       init = function()
-           vim.g.rustfmt_autosave = 1
-       end,
-   },
-
-   -- Here is a more advanced example where we pass configuration
-   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-   --    require('gitsigns').setup({ ... })
-   --
-   -- See `:help gitsigns` to understand what the configuration keys do
-   { -- Adds git related signs to the gutter, as well as utilities for managing changes
-       'lewis6991/gitsigns.nvim',
-       opts = {
-           signs = {
-               add = { text = '+' },
-               change = { text = '~' },
-               delete = { text = '_' },
-               topdelete = { text = '‾' },
-               changedelete = { text = '~' },
-           },
-       },
-   },
-   { -- Collection of various small independent plugins/modules
-       'echasnovski/mini.nvim',
-       config = require 'configs.mini',
-   },
-
-   -- DB
-   {
-       "kndndrj/nvim-dbee",
-       dependencies = {
-           "MunifTanjim/nui.nvim",
-       },
-       build = function()
-           -- Install tries to automatically detect the install method.
-           -- if it fails, try calling it with one of these parameters:
-           --    "curl", "wget", "bitsadmin", "go"
-           require("dbee").install()
-       end,
-   },
-
-    -- -- Debug
-    -- {
-    --     -- NOTE: Yes, you can install new plugins here!
-    --     'mfussenegger/nvim-dap',
-    --     -- NOTE: And you can specify dependencies as well
-    --     dependencies = {
-    --         -- Creates a beautiful debugger UI
-    --         'rcarriga/nvim-dap-ui',
-    --
-    --         -- Required dependency for nvim-dap-ui
-    --         'nvim-neotest/nvim-nio',
-    --
-    --         -- Installs the debug adapters for you
-    --         'williamboman/mason.nvim',
-    --         'jay-babu/mason-nvim-dap.nvim',
-    --
-    --         -- Add your own debuggers here
-    --         'leoluz/nvim-dap-go',
-    --     },
-    --     keys = function(_, keys)
-    --         require('configs.dap').keys(_, keys)
-    --     end,
-    --     config = function()
-    --         require('configs.dap').config()
-    --     end,
-    -- },
+--   -- Telescope
+--   {
+--       'nvim-telescope/telescope.nvim',
+--       event = 'VimEnter',
+--       branch = '0.1.x',
+--       dependencies = {
+--           'nvim-lua/plenary.nvim',
+--           {
+--               'nvim-telescope/telescope-fzf-native.nvim',
+--               -- NOTE: If you are having trouble with this installation,
+--               --       refer to the README for telescope-fzf-native for more instructions.
+--               build = 'make',
+--               cond = function()
+--                   return vim.fn.executable 'make' == 1
+--               end,
+--           },
+--           { 'nvim-telescope/telescope-ui-select.nvim' },
+--
+--           -- Useful for getting pretty icons, but requires a Nerd Font.
+--           { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+--       },
+--   },
+--
+--   -- Codeium completion
+--   {
+--       'Exafunction/codeium.vim',
+--   },
+--
+--   --Null-ls
+--   {
+--       'jose-elias-alvarez/null-ls.nvim',
+--       event = 'VeryLazy',
+--   },
+--
+--   -- Git related plugins
+--   { 'tpope/vim-fugitive' },
+--
+--   -- Nvim tree
+--   {
+--       'nvim-tree/nvim-tree.lua',
+--       dependencies = { 'nvim-tree/nvim-web-devicons' },
+--       cmd = { 'NvimTreeToggle', 'NvimTreeFocus' },
+--   },
+--
+--   -- Fancey vim
+--   {
+--       'eandrju/cellular-automaton.nvim',
+--   },
+--
+--   -- presence
+--   {
+--       'andweeb/presence.nvim',
+--   },
+--
+--   {
+--       'vyfor/cord.nvim',
+--       build = './build',
+--       event = 'VeryLazy',
+--       opts = {},
+--   },
+--
+--   -- LSP
+--   {
+--       'neovim/nvim-lspconfig',
+--       dependencies = {
+--           -- Automatically install LSPs and related tools to stdpath for neovim
+--           'williamboman/mason.nvim',
+--           'williamboman/mason-lspconfig.nvim',
+--           'WhoIsSethDaniel/mason-tool-installer.nvim',
+--           'MunifTanjim/prettier.nvim',
+--
+--           -- Useful status updates for LSP.
+--           -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
+--           { 'j-hui/fidget.nvim', opts = {} },
+--       },
+--   },
+--   {
+--       -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
+--       -- used for completion, annotations and signatures of Neovim apis
+--       'folke/lazydev.nvim',
+--       ft = 'lua',
+--       opts = {
+--           library = {
+--               -- Load luvit types when the `vim.uv` word is found
+--               { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+--           },
+--       },
+--   },
+--   { 'Bilal2453/luvit-meta', lazy = true },
+--
+--   -- Autocompletion
+--   {
+--       'hrsh7th/nvim-cmp',
+--       event = 'InsertEnter',
+--       dependencies = {
+--           -- Snippet Engine & its associated nvim-cmp source
+--           {
+--               'L3MON4D3/LuaSnip',
+--               build = (function()
+--                   -- Build Step is needed for regex support in snippets
+--                   -- This step is not supported in many windows environments
+--                   -- Remove the below condition to re-enable on windows
+--                   if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+--                       return
+--                   end
+--                   return 'make install_jsregexp'
+--               end)(),
+--           },
+--           -- LSP Support
+--           { 'neovim/nvim-lspconfig' },
+--           { 'williamboman/mason.nvim' },
+--           { 'williamboman/mason-lspconfig.nvim' },
+--
+--           -- Autocompletion
+--           { 'hrsh7th/cmp-buffer' },
+--           { 'hrsh7th/cmp-path' },
+--           { 'hrsh7th/cmp-cmdline' },
+--           { 'saadparwaiz1/cmp_luasnip' },
+--           { 'hrsh7th/cmp-nvim-lsp' },
+--           { 'hrsh7th/cmp-nvim-lua' },
+--
+--           -- Snippets
+--           { 'L3MON4D3/LuaSnip' },
+--           { 'rafamadriz/friendly-snippets' },
+--       },
+--   },
+--
+--   -- Tmux navigate
+--   {
+--       'christoomey/vim-tmux-navigator',
+--       lazy = false,
+--   },
+--
+--   -- Detect tabstop and shiftwidth automatically
+--   {
+--       'ThePrimeagen/refactoring.nvim',
+--       dependencies = {
+--           'nvim-lua/plenary.nvim',
+--           'nvim-treesitter/nvim-treesitter',
+--       },
+--   },
+--
+--   {
+--       -- Add indentation guides even on blank lines
+--       'lukas-reineke/indent-blankline.nvim',
+--       -- Enable `lukas-reineke/indent-blankline.nvim`
+--       -- See `:help ibl`
+--       main = 'ibl',
+--       opts = {},
+--   },
+--   { -- Linting
+--       'mfussenegger/nvim-lint',
+--       event = { 'BufReadPre', 'BufNewFile' },
+--       config = function()
+--           require 'configs.linting'
+--       end,
+--   },
+--
+--   { -- Autoformat
+--       'stevearc/conform.nvim',
+--       event = { 'BufWritePre' },
+--       cmd = { 'ConformInfo' },
+--       keys = {
+--           {
+--               '<leader>f',
+--               function()
+--                   require('conform').format { async = true, lsp_format = 'fallback' }
+--               end,
+--               mode = '',
+--               desc = '[F]ormat buffer',
+--           },
+--       },
+--   },
+--
+--   { -- Collection of various small independent plugins/modules
+--       'echasnovski/mini.nvim',
+--       configs = require 'configs.mini',
+--   },
+--   {                       -- Useful plugin to show you pending keybinds.
+--       'folke/which-key.nvim',
+--       event = 'VimEnter', -- Sets the loading event to 'VimEnter'
+--   },
+--
+--   {
+--       'xiyaowong/transparent.nvim',
+--   },
+--
+--   {
+--       'letieu/hacker.nvim',
+--   },
+--   {
+--       'ty-labs/randiverse.nvim',
+--       version = '*', -- Use for stability; omit to use `main` branch for the latest features
+--   },
+--   -- Auto pairs
+--   -- {
+--   --     "windwp/nvim-autopairs",
+--   --     event = "InsertEnter",
+--   --     opts = {}, -- this is equalent to setup({}) function
+--   -- },
+--   -- "tmsvg/pear-tree",
+--
+--   -- Auto tag
+--   {
+--       'windwp/nvim-ts-autotag',
+--   },
+--
+--   -- selicon snap
+--   {
+--       'michaelrommel/nvim-silicon',
+--       lazy = true,
+--       cmd = 'Silicon',
+--   },
+--   -- {
+--   --   'iamcco/markdown-preview.nvim',
+--   --   cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+--   --   ft = { 'markdown' },
+--   --   build = function()
+--   --     vim.fn['mkdp#util#install']()
+--   --   end,
+--   -- },
+--   {
+--       'davidmh/mdx.nvim',
+--       config = true,
+--       dependencies = { 'nvim-treesitter/nvim-treesitter' },
+--   },
+--   {
+--       'epwalsh/obsidian.nvim',
+--       version = '*', -- recommended, use latest release instead of latest commit
+--       lazy = true,
+--       ft = 'markdown',
+--       -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+--       -- event = {
+--       --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+--       --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+--       --   -- refer to `:h file-pattern` for more examples
+--       --   "BufReadPre path/to/my-vault/*.md",
+--       --   "BufNewFile path/to/my-vault/*.md",
+--       -- },
+--       dependencies = {
+--           -- Required.
+--           'nvim-lua/plenary.nvim',
+--
+--           -- see below for full list of optional dependencies 👇
+--       },
+--   },
+--   {
+--       'rust-lang/rust.vim',
+--       ft = 'rust',
+--       init = function()
+--           vim.g.rustfmt_autosave = 1
+--       end,
+--   },
+--
+--   -- Here is a more advanced example where we pass configuration
+--   -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+--   --    require('gitsigns').setup({ ... })
+--   --
+--   -- See `:help gitsigns` to understand what the configuration keys do
+--   { -- Adds git related signs to the gutter, as well as utilities for managing changes
+--       'lewis6991/gitsigns.nvim',
+--       opts = {
+--           signs = {
+--               add = { text = '+' },
+--               change = { text = '~' },
+--               delete = { text = '_' },
+--               topdelete = { text = '‾' },
+--               changedelete = { text = '~' },
+--           },
+--       },
+--   },
+--   { -- Collection of various small independent plugins/modules
+--       'echasnovski/mini.nvim',
+--       config = require 'configs.mini',
+--   },
+--
+--   -- DB
+--   {
+--       "kndndrj/nvim-dbee",
+--       dependencies = {
+--           "MunifTanjim/nui.nvim",
+--       },
+--       build = function()
+--           -- Install tries to automatically detect the install method.
+--           -- if it fails, try calling it with one of these parameters:
+--           --    "curl", "wget", "bitsadmin", "go"
+--           require("dbee").install()
+--       end,
+--   },
+--
+--    -- -- Debug
+--    -- {
+--    --     -- NOTE: Yes, you can install new plugins here!
+--    --     'mfussenegger/nvim-dap',
+--    --     -- NOTE: And you can specify dependencies as well
+--    --     dependencies = {
+--    --         -- Creates a beautiful debugger UI
+--    --         'rcarriga/nvim-dap-ui',
+--    --
+--    --         -- Required dependency for nvim-dap-ui
+--    --         'nvim-neotest/nvim-nio',
+--    --
+--    --         -- Installs the debug adapters for you
+--    --         'williamboman/mason.nvim',
+--    --         'jay-babu/mason-nvim-dap.nvim',
+--    --
+--    --         -- Add your own debuggers here
+--    --         'leoluz/nvim-dap-go',
+--    --     },
+--    --     keys = function(_, keys)
+--    --         require('configs.dap').keys(_, keys)
+--    --     end,
+--    --     config = function()
+--    --         require('configs.dap').config()
+--    --     end,
+--    -- },
 }
 
 require('lazy').setup(plugins, {
