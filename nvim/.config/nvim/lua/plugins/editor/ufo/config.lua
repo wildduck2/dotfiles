f1cae7f3626aabe5d7e107d7b9f277bf614b0db5
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-04-20 10:06:36 +0200

    refactor(nvim): move ufo fold settings to init hook, drop BufReadPost autocmd

--- nvim/.config/nvim/lua/plugins/editor/ufo/config.lua
+++ nvim/.config/nvim/lua/plugins/editor/ufo/config.lua
@@ -1,18 +1,6 @@
 local M = {}
 
 function M.setup()
-  -- Fold display settings
-  -- '0'=hide, '1'=show fold column (1 char wide)
-  vim.o.foldcolumn = '1'
-  -- false: start with all folds open
-  vim.o.foldenable = false
-  -- max nesting depth for folds (99 = effectively all)
-  vim.o.foldlevel = 99
-  -- foldlevel when opening a buffer (99 = all open)
-  vim.o.foldlevelstart = 99
-  -- chars for fold markers and diff display
-  vim.o.fillchars = [[fold: ,foldopen:-,foldsep:│,diff:⣿]]
-
   require('ufo').setup {
     open_fold_hl_timeout = 150,
     close_fold_kinds_for_ft = {},
@@ -20,15 +8,6 @@ function M.setup()
       return { 'treesitter', 'indent' }
     end,
   }
-
-  -- Open all folds when entering a buffer
-  vim.api.nvim_create_autocmd('BufReadPost', {
-    callback = function()
-      vim.defer_fn(function()
-        pcall(require('ufo').openAllFolds)
-      end, 50)
-    end,
-  })
 
   -- Fold keymaps
   vim.keymap.set('n', '<leader>fR', require('ufo').openAllFolds)
--- nvim/.config/nvim/lua/plugins/editor/ufo/init.lua
+++ nvim/.config/nvim/lua/plugins/editor/ufo/init.lua
@@ -2,6 +2,13 @@ return {
   'kevinhwang91/nvim-ufo',
   event = 'BufReadPost',
   dependencies = { 'kevinhwang91/promise-async' },
+  init = function()
+    vim.o.foldcolumn = '1'
+    vim.o.foldenable = false
+    vim.o.foldlevel = 99
+    vim.o.foldlevelstart = 99
+    vim.o.fillchars = [[fold: ,foldopen:-,foldsep:│,diff:⣿]]
+  end,
   config = function()
     require('plugins.editor.ufo.config').setup()
   end,
