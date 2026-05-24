82b66fad62e88caaf1ced6038ffe5ee1385b38de
wildduck <108896341+wildduck2@users.noreply.github.com>
2026-05-24 19:13:39 +0300

    feat(nvim): keep fidget notifications clear of nvim-tree sidebar
    Set fidget notification.window.avoid={'NvimTree'} so progress popups
    never overlap the file tree. Replaces the implicit integration fidget
    plans to remove.

--- nvim/.config/nvim/lua/plugins/lsp/lspconfig/init.lua
+++ nvim/.config/nvim/lua/plugins/lsp/lspconfig/init.lua
@@ -4,7 +4,18 @@ return {
     { 'williamboman/mason.nvim', opts = {} },
     'williamboman/mason-lspconfig.nvim',
     'WhoIsSethDaniel/mason-tool-installer.nvim',
-    { 'j-hui/fidget.nvim', opts = {} },
+    {
+      'j-hui/fidget.nvim',
+      opts = {
+        notification = {
+          window = {
+            -- Avoid the nvim-tree sidebar so fidget never overlaps it.
+            -- Replaces the implicit integration that fidget plans to remove.
+            avoid = { 'NvimTree' },
+          },
+        },
+      },
+    },
   },
   config = function()
     require('plugins.lsp.lspconfig.config').setup()
